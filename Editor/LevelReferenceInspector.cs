a982c9640f4a96e54b21e492af710a87e209de76
Castle <castle.wsc@gmail,com>
2021-06-02 20:08:59 +0800

    update to 1.2.2
    add LevelReference

---
+++ Editor/LevelReferenceInspector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Lab5Games.Editor
+{
+    [CustomEditor(typeof(LevelReference))]
+    public class LevelReferenceInspector : UnityEditor.Editor
+    {
+        
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            GUILayout.Space(25);
+
+            if (GUILayout.Button("Build Levels"))
+            {
+                (target as LevelReference).BuildLevels();
+            }
+        }
+    }
+}
---
+++ Runtime/LevelReference.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using System.IO;
+using UnityEngine.SceneManagement;
+#endif
+
+namespace Lab5Games
+{
+    [CreateAssetMenu(fileName ="New Level Reference", menuName ="Lab5Games/Level Reference")]
+    public class LevelReference : ScriptableObject
+    {
+        [System.Serializable]
+        public class Level
+        {
+            [SerializeField] string sceneName;
+            [SerializeField] string sceneGUID;
+            [SerializeField] int sceneBuildIndex;
+            [SerializeField] string path;
+
+            public string GUID
+            {
+                get { return this.sceneGUID; }
+                internal set { this.sceneGUID = value; }
+            }
+
+            public string Path
+            {
+                get { return this.path; }
+                internal set { this.path = value; }
+            }
+
+            public string SceneName
+            {
+                get { return this.sceneName; }
+                internal set { this.sceneName = value; }
+            }
+
+            public int SceneBuildIndex
+            {
+                get { return this.sceneBuildIndex; }
+                internal set { this.sceneBuildIndex = value; }
+            }
+        }
+
+
+#if UNITY_EDITOR
+        [SerializeField] SceneAsset[] _sceneAssets;
+
+        public void BuildLevels()
+        {
+            if (_sceneAssets == null || _sceneAssets.Length <= 0)
+            {
+                return;
+            }
+
+            _levels = new Level[_sceneAssets.Length];
+
+            for (int i = 0; i < _sceneAssets.Length; i++)
+            {
+                string path = AssetDatabase.GetAssetPath(_sceneAssets[i]);
+                string guid = AssetDatabase.AssetPathToGUID(path);
+                string sceneName = Path.GetFileNameWithoutExtension(path);
+                int sceneBuildIndex = SceneUtility.GetBuildIndexByScenePath(path);
+
+                Level newLevel = new Level();
+                newLevel.GUID = guid;
+                newLevel.Path = path;
+                newLevel.SceneName = sceneName;
+                newLevel.SceneBuildIndex = sceneBuildIndex;
+
+                _levels[i] = newLevel;
+
+                DebugEx.Log(ELogType.Trace, $"Build level...({path}).");
+            }
+
+            EditorUtility.SetDirty(this);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+#endif
+
+        [SerializeField] Level[] _levels;
+
+        public Level[] levels
+        {
+            get { return _levels; }
+        }
+    }
+}
