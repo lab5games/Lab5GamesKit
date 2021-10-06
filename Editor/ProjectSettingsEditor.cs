3016aa2dca3d7a030083391e80e2cc3338d09964
Castle <castle.wsc@gmail,com>
2021-10-06 14:13:40 +0800

    update tot 1.3.8
    1. remove build version process
    2. auto add debug script define when project created

--- Editor/ProjectSettingsEditor.cs
+++ Editor/ProjectSettingsEditor.cs
@@ -14,8 +14,6 @@ namespace Lab5Games.Editor
         }
 
         string _projectName = "New Project";
-        bool _debugMode = false;
-        bool _autoBuildVersion = false;
 
 
         readonly string[] SUB_FOLDERS = new string[]
@@ -34,22 +32,12 @@ namespace Lab5Games.Editor
         };
 
         const string DEBUG_MODE_SCRIPT_DEFINE_SYMBOL = "DEBUG_MODE";
-        const string DEBUG_MODE_PLAYERPREFS_KEY = "debug_mode";
-        const string AUTO_BUILD_VERSION_PLAYERPRES_KEY = "auto_build_version";
-
-        void OnEnable()
-        {
-            _debugMode = IsDebugMode();
-            _autoBuildVersion = IsAutoBuildVersion();
-        }
 
         void OnGUI()
         {
             EditorGUILayout.Space();
 
             _projectName = EditorGUILayout.TextField("Project Name", _projectName);
-            ToggleDebugMode(EditorGUILayout.Toggle("Debug Mode", _debugMode));
-            ToggleAutoBuildVersion(EditorGUILayout.Toggle("Auto Build Version", _autoBuildVersion));
 
             EditorGUILayout.Space(20);
             if(GUILayout.Button("New Porject"))
@@ -80,53 +68,21 @@ namespace Lab5Games.Editor
                     AssetDatabase.CreateFolder(root, SUB_FOLDERS[i]);
                 }
 
-                BuildVersionProcess.ResetBuildVersion();
+                // add debug define symbol
+                string strDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+
+                List<string> allDefines = strDefines.Split(';').ToList();
+
+                if (!allDefines.Contains(DEBUG_MODE_SCRIPT_DEFINE_SYMBOL))
+                    allDefines.Add(DEBUG_MODE_SCRIPT_DEFINE_SYMBOL);
+
+                PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, string.Join(";", allDefines.ToArray()));
+
+
+                AssetDatabase.Refresh();
+
+                Debug.Log($"{_projectName} project created");
             }
-        }
-
-        private void ToggleDebugMode(bool toggle)
-        {
-            if (_debugMode != toggle)
-            {
-                _debugMode = toggle;
-                PlayerPrefs.SetInt(DEBUG_MODE_PLAYERPREFS_KEY, toggle ? 1 : 0);
-
-                string definesString = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
-
-                List<string> allDefines = definesString.Split(';').ToList();
-
-                if (toggle)
-                {
-                    if (!allDefines.Contains(DEBUG_MODE_SCRIPT_DEFINE_SYMBOL)) allDefines.Add(DEBUG_MODE_SCRIPT_DEFINE_SYMBOL);
-                }
-                else
-                {
-                    if (allDefines.Contains(DEBUG_MODE_SCRIPT_DEFINE_SYMBOL)) allDefines.Remove(DEBUG_MODE_SCRIPT_DEFINE_SYMBOL);
-                }
-
-                PlayerSettings.SetScriptingDefineSymbolsForGroup(
-                        EditorUserBuildSettings.selectedBuildTargetGroup,
-                        string.Join(";", allDefines.ToArray()));
-            }
-        }
-
-        private void ToggleAutoBuildVersion(bool toggle)
-        {
-            if(_autoBuildVersion != toggle)
-            {
-                _autoBuildVersion = toggle;
-                PlayerPrefs.SetInt(AUTO_BUILD_VERSION_PLAYERPRES_KEY, toggle ? 1 : 0);
-            }
-        }
-
-        public static bool IsDebugMode()
-        {
-            return PlayerPrefs.GetInt(DEBUG_MODE_PLAYERPREFS_KEY, 0) == 1;
-        }
-
-        public static bool IsAutoBuildVersion()
-        {
-            return PlayerPrefs.GetInt(AUTO_BUILD_VERSION_PLAYERPRES_KEY, 0) == 1;
         }
     }
 }
