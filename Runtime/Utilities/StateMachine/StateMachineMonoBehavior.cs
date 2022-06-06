2e75ddd881dcb0da490b228bbfcdc2b830638654
Castle Wu <castle.wsc@gmail.com>
2022-06-06 17:21:05 +0800

    update

---
+++ Runtime/Utilities/StateMachine/StateMachineMonoBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Lab5Games.StateMachine
+{
+    public abstract class StateMachineMonoBehavior : MonoBehaviour
+    {
+        protected StateMachine<StateMonoBehavior> FSM;
+
+        protected virtual void Start()
+        {
+            FSM = new StateMachine<StateMonoBehavior>();
+
+            foreach(var state in GetComponentsInChildren<StateMonoBehavior>())
+            {
+                FSM.AddState(state);
+            }
+        }
+
+        protected void Update()
+        {
+            FSM.UpdateStateMachine(Time.deltaTime);
+        }
+    }
+}
---
+++ Runtime/Utilities/StateMachine/StateMonoBehavior.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Lab5Games.StateMachine
+{
+    public abstract class StateMonoBehavior : MonoBehaviour, IState
+    {
+        public abstract void EnterState(float dt, IState fromState);
+
+        public abstract void ExitState(float dt, IState toState);
+
+        public abstract void UpdateState(float dt);
+
+        public abstract bool CheckTransition(IState toState);
+    }
+}
