d3f45b1196c1c886107e49ddd67650e535699c0d
Castle Wu <castle.wsc@gmail.com>
2022-06-14 17:53:13 +0800

    update

--- Runtime/Utilities/StateMachine/StateMachineMonoBehavior.cs
+++ Runtime/Utilities/StateMachine/StateMachineMonoBehavior.cs
@@ -6,13 +6,31 @@ namespace Lab5Games.StateMachine
     {
         protected StateMachine<StateMonoBehavior> FSM;
 
+        public StateMonoBehavior CurrentState => FSM.CurrentState;
+        public StateMonoBehavior PreviousState => FSM.PreviousState;
+
+        public string[] GetStateNames() => FSM.GetStateNames();
+
+        public StateMonoBehavior GetState(string stateName) => FSM.GetState(stateName);
+
+        public void EnqueueTransition(string stateName) => FSM.EnqueueTransition(stateName);
+
+        public void EnqueueTransition(StateMonoBehavior state) => FSM.EnqueueTransition(state);
+
+        public void ForceState(string stateName) => FSM.ForceState(stateName);
+
+        public void ForceState(StateMonoBehavior state) => FSM.ForceState(state);
+
+        public void AddState(StateMonoBehavior state) => FSM.AddState(state);
+
+
         protected virtual void Start()
         {
             FSM = new StateMachine<StateMonoBehavior>();
 
             foreach(var state in GetComponentsInChildren<StateMonoBehavior>())
             {
-                FSM.AddState(state);
+                AddState(state);
             }
         }
 
