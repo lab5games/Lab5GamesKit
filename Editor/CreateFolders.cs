2006335576065f62638cf2792132fce7887b0c1a
Castle Wu <castle.wsc@gmail.com>
2022-07-10 11:36:40 +0800

    update 2.3.1
    add create default project folder editor script

---
+++ Editor/CreateFolders.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+
+namespace Lab5Games.Editor
+{
+    public class CreateFolders : OdinEditorWindow
+    {
+        [ShowInInspector]
+        public static string projectName = "PROJECT_NAME";
+
+        [MenuItem("Assets/Create Default Folders")]
+        static void OpenWindow()
+        {
+            var window = GetWindow<CreateFolders>();
+
+            window.position = new Rect(Screen.width / 2, Screen.height / 2, 400, 150);
+            window.ShowPopup();
+        }
+
+        static void CreateAllFolders()
+        {
+            List<string> folders = new List<string>()
+            { 
+                "Art/Animations",
+                "Art/Materials",
+                "Art/Models",
+                "Art/Textures",
+
+                "Audio/Music",
+                "Audio/Sound",
+
+                "Code/Editor",
+                "Code/Scripts",
+                "Code/Shaders",
+
+                "Docs",
+
+                "Level/Fonts",
+                "Level/Prefabs",
+                "Level/Scenes",
+                "Level/Settings",
+                "Level/Scriptables",
+                "Level/ThirdParty",
+
+                "Level/UI/Assets",
+                "Level/UI/Icon",
+            };
+
+            if (string.IsNullOrEmpty(projectName))
+                projectName = "PROJECT_NAME";
+
+            foreach(string folder in folders)
+            {
+                if (!Directory.Exists("Assets/" + projectName + "/" + folder))
+                {
+                    Directory.CreateDirectory("Assets/" + projectName + "/" + folder);
+                }
+            }
+
+            AssetDatabase.Refresh();    
+        }
+
+        [Button(ButtonSizes.Large)]
+        void Generate()
+        {
+            CreateAllFolders();
+            Close();
+        }
+    }
+}
