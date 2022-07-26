a5e173a01d08a76a86126130356f00e0a4a16981
Castle Wu <castle.wsc@gmail.com>
2022-07-26 14:27:57 +0800

    update 2.4.4

--- Runtime/Utilities/BitUtils.cs
+++ Runtime/Utilities/BitUtils.cs
@@ -1,68 +1,104 @@
 ﻿using System;
 using System.Text;
+using System.Linq;
 
 namespace Lab5Games
 {
     public static class BitUtils
     {
-        public static string GetIntBinaryString(int value)
+        public static string ToBinaryStringFromByte(byte x)
         {
-            StringBuilder sb = new StringBuilder(32);
+            char[] buff = new char[8];
 
-            int pos = 31;
-            int i= 0;
-
-            while(i<32)
+            for(int i=7; i>=0; i--)
             {
-                if ((value & (1 << i)) != 0)
-                    sb.Append('1');
-                else
-                    sb.Append('0');
-                pos--;
-                i++;
+                int mask = 1 << i;
+                buff[7 - i] = (x & mask) != 0 ? '1' : '0';
             }
 
-            return sb.ToString();   
-        }
+            int cut_point = 4;
+            StringBuilder sb = new StringBuilder(9);
+            
+            foreach(var b in buff)
+            {
+                if(cut_point == 0)
+                {
+                    cut_point = 4;
+                    sb.Append(" ");
+                }
 
-        public static string GetByteBinaryString(byte value)
-        {
-            StringBuilder sb = new StringBuilder(8);
-
-            for (int indx = 0; indx < 8; indx++)
-                sb.Append(GetBit(value, indx) ? '1' : '0');
+                sb.Append(b);
+                --cut_point;
+            }
 
             return sb.ToString();
         }
 
-        public static bool GetBit(this int value, int position)
+        public static string ToBinaryStringFromInteger(int x)
         {
-            return (value & (1 << position)) != 0;
-        }
+            char[] buff = new char[32];
 
-        public static bool GetBit(this byte value, int position)
-        {
-            return (value & (1 << position)) != 0;
-        }
-
-        public static int SetBit(this int value, int position, bool bit)
-        {
-            return bit ? value |= (1 << position) : value & ~(1 << position); 
-        }
-
-        public static byte SetBit(this byte value, int position, bool bit)
-        {
-            byte result;
-            if(bit)
+            for(int i=31; i>=0; i--)
             {
-                result = Convert.ToByte(value | (1 << position));
-            }
-            else
-            {
-                result = Convert.ToByte(value & ~(1<<position));
+                int mask = 1 << i;
+                buff[31 - i] = (x & mask) != 0 ? '1' : '0';
             }
 
-            return result;
+            int cut_point = 4;
+            StringBuilder sb = new StringBuilder(39);
+
+            foreach (var b in buff)
+            {
+                if (cut_point == 0)
+                {
+                    cut_point = 4;
+                    sb.Append(" ");
+                }
+
+                sb.Append(b);
+                --cut_point;
+            }
+
+            return sb.ToString();
+        }
+
+        public static int SetBit(this int x, int pos, int flag)
+        {
+            if (pos < 0 || pos > 32)
+                throw new ArgumentOutOfRangeException();
+
+            int mask = 1 << pos;
+            return ((x & ~mask) | ((flag > 0 ? 1 : 0) << pos));
+        }
+
+        public static int GetBit(this int x, int pos)
+        {
+            if (pos < 0 || pos > 32)
+                throw new ArgumentOutOfRangeException();
+
+            int mask = 1 << pos;
+            return (x & mask) != 0 ? 1 : 0;
+        }
+
+        public static byte ConvertToByte(string inputStr)
+        {
+            string str = new string(inputStr.ToCharArray()
+                .Where(c => !char.IsWhiteSpace(c))
+                .ToArray());
+
+            return Convert.ToByte(str, 2);
+        }
+
+        public static byte SetBit(this byte x, int pos, int flag)
+        {
+            return flag > 0 ?
+                Convert.ToByte(x | (1 << pos)) :
+                Convert.ToByte(x & ~(1 << pos));
+        }
+
+        public static int GetBit(this byte x, int pos)
+        {
+            return (x & (1 << pos)) != 0 ? 1 : 0;
         }
     }
 }
